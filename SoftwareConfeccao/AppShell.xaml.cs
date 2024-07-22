460fc119490717c28ad3744abb308bcae7cdc410
Guilherme <gui.igarashi@hotmail.com>
2024-07-22 09:57:18 -0300

    Commit Inicial

---
+++ SoftwareConfeccao/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SoftwareConfeccao;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ SoftwareConfeccao/Pessoa.cs
@@ -0,0 +1,40 @@
+public class Pessoa
+{
+    string nome;
+    int telefone;
+
+    string email;
+
+
+    public void SetNome(string nome)
+    {
+        this.nome = nome;
+    }
+
+    public string GetNome()
+    {
+        return nome;
+    }
+
+    public void SetTelefone(int telefone)
+    {
+        this.telefone = telefone;
+    }    
+
+    public int GetTelefone()
+    {
+        return telefone;
+    }
+
+    public void SetEmail(string email)
+    {
+        this.email = email;
+    }    
+
+    public string GetTelefone()
+    {
+        return email;
+    }
+
+}
+
