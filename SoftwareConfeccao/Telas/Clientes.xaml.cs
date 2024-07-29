cfbf01dd9c0ebbca526fa2944bc61b5fbaf8886a
Guilherme <gui.igarashi@hotmail.com>
2024-07-29 11:49:16 -0300

    dia produtivo

---
+++ SoftwareConfeccao/Telas/Clientes.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.Maui.Controls;
+
+namespace Telas
+{
+    public partial class Clientes
+    {
+        public Clientes()
+        {
+            InitializeComponent();
+        }
+
+        // Aqui você pode adicionar manipuladores de eventos para os botões, se necessário.
+    }
+}
---
+++ SoftwareConfeccao/Telas/ClientesAltera.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.Maui.Controls;
+
+namespace Telas
+{
+    public partial class ClientesAltera
+    {
+        public ClientesAltera()
+        {
+            InitializeComponent();
+        }
+
+        // Aqui você pode adicionar manipuladores de eventos para os botões, se necessário.
+    }
+}
