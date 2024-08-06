4ee3239cc81bac623ae518a2c996b4fe1a9b41cc
Guilherme <gui.igarashi@hotmail.com>
2024-08-06 11:22:17 -0300

    tive que reiniciar o pc

---
+++ SoftwareConfeccao/Telas/Pedido.xaml.cs
@@ -0,0 +1,12 @@
+namespace Telas
+{
+    public partial class Pedidos
+    {
+        public Pedidos()
+        {
+            InitializeComponent();
+        }
+
+        // Aqui você pode adicionar manipuladores de eventos para os botões, se necessário.
+    }
+}
