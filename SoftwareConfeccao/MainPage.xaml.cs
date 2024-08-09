8acd7cf8ac9029e5d40daa2c6b7a665489f5b514
Guilherme <gui.igarashi@hotmail.com>
2024-08-09 11:11:06 -0300

    tela inicial e começa a fazer as âncoras

--- SoftwareConfeccao/MainPage.xaml.cs
+++ SoftwareConfeccao/MainPage.xaml.cs
@@ -7,6 +7,46 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 	}
+       private void OnClienteClicked(object sender, EventArgs e)
+        {
+            // Código para quando o botão "Cliente" for clicado
+            DisplayAlert("Cliente", "Você clicou em Cliente", "OK");
+        }
 
+        private void OnFornecedorClicked(object sender, EventArgs e)
+        {
+            // Código para quando o botão "Fornecedor" for clicado
+            DisplayAlert("Fornecedor", "Você clicou em Fornecedor", "OK");
+        }
+
+        private void OnTransportadoraClicked(object sender, EventArgs e)
+        {
+            // Código para quando o botão "Transportadora" for clicado
+            DisplayAlert("Transportadora", "Você clicou em Transportadora", "OK");
+        }
+
+        private void OnMateriaPrimaClicked(object sender, EventArgs e)
+        {
+            // Código para quando o botão "Matéria Prima" for clicado
+            DisplayAlert("Matéria Prima", "Você clicou em Matéria Prima", "OK");
+        }
+
+        private void OnProdutoClicked(object sender, EventArgs e)
+        {
+            // Código para quando o botão "Produto" for clicado
+            DisplayAlert("Produto", "Você clicou em Produto", "OK");
+        }
+
+        private void OnPedidosClicked(object sender, EventArgs e)
+        {
+            // Código para quando o botão "Pedidos" for clicado
+            DisplayAlert("Pedidos", "Você clicou em Pedidos", "OK");
+        }
+
+        private void OnEstoqueClicked(object sender, EventArgs e)
+        {
+            // Código para quando o botão "Estoque" for clicado
+            DisplayAlert("Estoque", "Você clicou em Estoque", "OK");
+        }
 }
 
---
+++ SoftwareConfeccao/Telas/TransportadoraCadastro.xaml.cs
@@ -0,0 +1,12 @@
+namespace Telas
+{
+    public partial class TransportadoraCadastro
+    {
+        public TransportadoraCadastro()
+        {
+            InitializeComponent();
+        }
+
+        // Aqui você pode adicionar manipuladores de eventos para os botões, se necessário.
+    }
+}
